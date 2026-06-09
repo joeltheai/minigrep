72c35e271fd4ace973959f7d5e2c275eac2b054a
joeltheai <55612521+joeltheai@users.noreply.github.com>
2026-06-09 15:13:21 +0530

    args read

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let ar: Vec<String> = env::args().collect();
+    dbg!(&ar);
+
+    let query = &ar[1];
+    let file_path = &ar[2];
+
+    println!("Searching for {query}");
+    println!("In file {file_path}");
+    // println!("{:?}", ar);
 }
