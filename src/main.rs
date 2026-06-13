a9805cf6222c8dd1cfe78d13191daa75f69b017b
joeltheai <55612521+joeltheai@users.noreply.github.com>
2026-06-13 20:17:57 +0530

    stuff

--- src/main.rs
+++ src/main.rs
@@ -25,12 +25,11 @@ fn main() {
         Ok(contents) => {
             println!("With text:\n{contents}");
         }
-        // Err(err) if err.kind() == ErrorKind::NotFound => {
-        //     eprintln!("Error: The file '{}' does not exist.", file_path);
-        //     // Program exits cleanly here instead of crashing with a panic
-        // }
         Err(other_err) => {
             eprintln!("{}", other_err);
-        }
+        } // Err(err) if err.kind() == ErrorKind::NotFound => {
+          //     eprintln!("Error: The file '{}' does not exist.", file_path);
+          //     // Program exits cleanly here instead of crashing with a panic
+          // }
     }
 }
